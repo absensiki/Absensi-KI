8b4de88aed562c724ac2bd4f80b5c3eb41d3057e
absensiki <absensiki@gmail.com>
2026-01-20 20:35:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-# Tambahkan ini di bagian atas dekat import
-import time
-
-# Jika jam di website meleset, kita tambahkan offset 7 jam untuk WIB
-jam_sekarang = (datetime.datetime.now() + datetime.timedelta(hours=7)).hour
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
@@ -76,6 +71,7 @@ with tab2:
     st.subheader("Data Absen Terkini")
     rekap = conn.read(spreadsheet=LINK_SHEET)
     st.dataframe(rekap)
+
 
 
 
