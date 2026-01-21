d95d6ee69829237462886a583c5a810e2a5b1aa6
absensiki <absensiki@gmail.com>
2026-01-21 10:28:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ waktu_wib = datetime.datetime.now() + datetime.timedelta(hours=7)
 jam_angka = waktu_wib.hour
 
 if 5 <= jam_angka < 11:
-    sapaan = "Halo, Selamat Pagi begundal 🌅"
+    sapaan = "Halo, Selamat Pagi 🌅"
 elif 11 <= jam_angka < 15:
     sapaan = "Halo, Selamat Siang ☀️"
 elif 15 <= jam_angka < 18:
@@ -83,6 +83,7 @@ with tab2:
         st.dataframe(data_rekap, use_container_width=True)
     except:
         st.info("Belum ada data.")
+
 
 
 
